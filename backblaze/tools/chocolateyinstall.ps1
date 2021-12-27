d7d5455d450e6c43b0071af51f42947b7780c124
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-12-27 13:42:40 -0500

    update backblaze

--- backblaze/tools/chocolateyinstall.ps1
+++ backblaze/tools/chocolateyinstall.ps1
@@ -1,5 +1,16 @@
+$pp = Get-PackageParameters
 
-$ErrorActionPreference = 'Stop';
+if (!$pp['UserEmail']) { $pp['UserEmail'] = Read-Host "Enter email address for your backblaze account:" }
+if (!$pp['Password']) { $pp['Password'] = Read-Host "Enter password for $($pp['UserEmail']):"}
+
+$silentArgs = "-nogui -installdir 'C:\Program Files\Backblaze' -signinaccount $($pp['UserEmail']) $($pp['Password'])"
+
+if (!$pp['Password']) { 
+  Write-Debug "Package needs parameter 'Password' to install, that must be provided in params or in prompt. Defaulting to GUI installation.." 
+  $silentArgs = ''
+}
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://secure.backblaze.com/win32/install_backblaze.exe'
 
@@ -8,31 +19,11 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-
-  softwareName  = 'Backblaze'
-
-  checksum      = 'EBF1739CC2BF49B16B79C81AD3BC5729'
-  checksumType  = 'md5'
-
+  softwareName  = 'backblaze*'
+  checksum      = '67C1927B9B6217E98FC03C91DFF241834DC735C31C77C2EE88FD48324746FC22'
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Install-ChocolateyPackage @packageArgs 
